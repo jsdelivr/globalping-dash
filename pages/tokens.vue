56f04ffc31d3d7d24590df0bcb2550fb8ab744cf
Alexey Yarmosh <yarmoshalexey@gmail.com>
2024-06-13 11:35:58 +0200

    feat: add empty tokens state

--- pages/tokens.vue
+++ pages/tokens.vue
@@ -1,19 +1,17 @@
 <template>
-	<div>
-		<h1>Tokens</h1>
-		<DataTable :value="tokens" table-style="min-width: 50rem">
-			<Column field="id" header="id"/>
-			<Column field="date_created" header="date_created"/>
-			<Column field="date_last_used" header="date_last_used"/>
-			<Column field="date_updated" header="date_updated"/>
-			<Column field="expire" header="expire"/>
-			<Column field="id" header="id"/>
-			<Column field="name" header="name"/>
-			<Column field="origins" header="origins"/>
-			<Column field="user_created" header="user_created"/>
-			<Column field="user_updated" header="user_updated"/>
-			<Column field="value" header="value"/>
-		</DataTable>
+	<div class="bg-surface-50 flex h-full flex-col p-6">
+		<div class="mb-4 flex">
+			<h1 class="col-span-2 text-2xl font-bold">Tokens</h1>
+			<Button class="ml-auto" label="Generate new token"/>
+		</div>
+		<p class="xl:w-1/2">Generate a token and add it to your Globalping requests to upper your hourly measurements limit. After the limit is exhausted, you can proceed with measurements by spending the earned credits.</p>
+		<div v-if="false" class="mt-6">data</div>
+		<div v-else class="bg-surface-0 mt-6 rounded-xl border px-4 py-3">
+			<div class="bg-surface-50 rounded-xl py-6 text-center">
+				<p class="font-semibold">No data to show</p>
+				<p class="mt-4">Adopt a probe or become a sponsor to track your credit usage</p>
+			</div>
+		</div>
 	</div>
 </template>
 

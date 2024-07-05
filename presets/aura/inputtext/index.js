dfc5eb7dc52c6ee3445672b9a53540764bc93256
Alexey Yarmosh <yarmoshalexey@gmail.com>
2024-07-05 11:17:05 +0200

    feat: udpate

--- presets/aura/inputtext/index.js
+++ presets/aura/inputtext/index.js
@@ -36,7 +36,7 @@ export default {
 			{
 				'hover:border-surface-400 dark:hover:border-surface-600': !context.disabled && !props.invalid,
 				'focus:outline-none focus:outline-offset-0 focus:ring-1 focus:ring-primary-500 dark:focus:ring-primary-400 focus:z-10': !context.disabled,
-				'text-surface-700 bg-surface-50 dark:bg-dark-500 select-none pointer-events-none cursor-default': context.disabled,
+				'!text-bluegray-500 bg-surface-50 dark:bg-dark-500 select-none pointer-events-none cursor-default': context.disabled,
 			},
 
 			// Filled State *for FloatLabel

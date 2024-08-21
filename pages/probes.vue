d3634b16107ac0db186727d87b5aaee133df75f6
Alexey Yarmosh <yarmoshalexey@gmail.com>
2024-08-21 18:35:38 +0200

    feat: remove expanded view from table

--- pages/probes.vue
+++ pages/probes.vue
@@ -10,7 +10,6 @@
 		</div>
 		<div v-if="probes.length || loading">
 			<DataTable
-				v-model:expandedRows="expandedRows"
 				:value="probes"
 				lazy
 				:first="first"
@@ -20,68 +19,14 @@
 				:loading="loading"
 				table-class="table-fixed"
 				:row-class="() => 'cursor-pointer hover:bg-surface-50 dark:hover:bg-dark-700'"
-				@row-click="toggleRow"
+				@row-click="openRow"
 			>
 				<template #header>
 					<h3>List of probes</h3>
 				</template>
 				<Column header="Name" class="w-1/4" body-class="!p-0">
 					<template #body="slotProps">
-						<div v-if="expandedRow === slotProps.data.id">
-							<div class="mx-2 grid grid-cols-[auto_1fr] grid-rows-[auto_auto] gap-x-3 pt-3">
-								<BigIcon class="col-span-1 row-span-2" :name="slotProps.data.hardwareDevice ? 'probe' : 'docker'" border :status="slotProps.data.status"/>
-								<div v-if="isEditingName" v-focustrap class="-mt-0.5 flex items-center border-b">
-									<InputText v-model="name" class="w-full rounded-none border-0 !bg-transparent !px-0 !py-1 font-bold" autofocus @keyup.enter="saveName(slotProps.data.id)"/>
-									<Button
-										icon="pi pi-check"
-										class="h-6 w-4 text-surface-900"
-										severity="secondary"
-										text
-										aria-label="Save name"
-										size="small"
-										@click="saveName(slotProps.data.id)"
-									/>
-									<Button
-										icon="pi pi-times"
-										class="h-6 w-4 text-surface-900"
-										severity="secondary"
-										text
-										aria-label="Close edit"
-										size="small"
-										@click="cancelName"
-									/>
-								</div>
-								<p v-else class="col-start-2 col-end-3 flex items-center font-bold">
-									{{ slotProps.data.name || slotProps.data.city }}
-									<Button
-										icon="pi pi-pencil"
-										class="mx-1 w-6 !py-0 text-surface-900"
-										severity="secondary"
-										text
-										aria-label="Edit name"
-										size="small"
-										@click="editName(slotProps.data.name)"
-									/>
-								</p>
-								<p class="col-start-2 col-end-3 row-start-2 row-end-3 text-bluegray-900 dark:text-bluegray-400">{{ slotProps.data.ip }}</p>
-							</div>
-							<div class="mb-6 mt-24 w-[200%] pl-16 pr-2">
-								<p class="border-b pb-2 font-bold">Probe details</p>
-								<div id="gp-map" class="mt-3 h-32 rounded-md"/>
-								<p class="mt-3">
-									Type:
-									<span class="ml-2 mr-8 font-bold">
-										{{ slotProps.data.hardwareDevice || 'Container' }}
-									</span>
-									Version
-									<span class="ml-2 font-bold">
-										{{ slotProps.data.version }}
-									</span>
-								</p>
-							</div>
-						</div>
 						<ProbeHeader
-							v-else
 							:name="slotProps.data.name || slotProps.data.city"
 							:ip="slotProps.data.ip"
 							:status="slotProps.data.status"
@@ -93,47 +38,7 @@
 				</Column>
 				<Column header="Location" class="w-1/4" body-class="!p-0">
 					<template #body="slotProps">
-						<div v-if="expandedRow === slotProps.data.id" class="px-2 py-3">
-							<div class="mb-1 flex items-center">
-								<CountryFlag :country="slotProps.data.country" size="small" class="px-6"/>
-								<div v-if="isEditingCity" v-focustrap class="-mt-0.5 ml-2 flex w-full items-center border-b">
-									<InputText v-model="city" class="w-full rounded-none border-0 !bg-transparent !px-0 !py-1 font-bold" autofocus @keyup.enter="saveCity(slotProps.data.id)"/>
-									<Button
-										icon="pi pi-check"
-										class="h-6 w-4 text-surface-900"
-										severity="secondary"
-										text
-										aria-label="Save city"
-										size="small"
-										@click="saveCity(slotProps.data.id)"
-									/>
-									<Button
-										icon="pi pi-times"
-										class="h-6 w-4 text-surface-900"
-										severity="secondary"
-										text
-										aria-label="Close edit"
-										size="small"
-										@click="cancelCity"
-									/>
-								</div>
-								<p v-else class="ml-2 font-bold">
-									{{ slotProps.data.city }}, {{ slotProps.data.country }}
-									<Button
-										icon="pi pi-pencil"
-										class="mx-1 w-6 !py-0 text-surface-900"
-										severity="secondary"
-										text
-										aria-label="Edit city"
-										size="small"
-										@click="editCity(slotProps.data.city)"
-									/>
-								</p>
-							</div>
-							<p>{{ slotProps.data.network }}, {{ slotProps.data.asn }}</p>
-							<p class="mt-3 text-2xs text-bluegray-400">City where the probe is located. If you know that city is wrong it can be changed here: type in the valid city and click save.</p>
-						</div>
-						<div v-else class="px-2 py-3">
+						<div class="px-2 py-3">
 							<div class="mb-1 flex items-center">
 								<CountryFlag :country="slotProps.data.country" size="small"/>
 								<p class="ml-2 font-bold">{{ slotProps.data.city }}, {{ slotProps.data.country }}</p>
@@ -144,72 +49,7 @@
 				</Column>
 				<Column header="Tags" class="w-[34%]" body-class="!p-0">
 					<template #body="slotProps">
-						<div v-if="expandedRow === slotProps.data.id" class="flex flex-col py-4">
-							<div class="px-2">
-								<div v-if="isEditingTags">
-									<div>
-										<div v-for="(tag, index) in tags" :key="index" class="mb-2 flex items-center" :class="{ 'mb-5': !isTagValid(tag.value) }">
-											<Select v-model="tag.uPrefix" class="grow" :options="uPrefixes" :scroll-height="'200px'"/>
-											<span class="mx-2">-</span>
-											<span class="relative grow">
-												<InputText v-model="tag.value" :invalid="!isTagValid(tag.value)" class="w-full"/>
-												<p v-if="!isTagValid(tag.value)" class="absolute pl-1 text-red-500">Invalid tag</p>
-											</span>
-											<Button icon="pi pi-trash" text aria-label="Remove" class="text-surface-900 dark:text-surface-0" @click="removeTag(index)"/>
-										</div>
-									</div>
-									<div class="mt-6 flex">
-										<Button
-											label="Add tag"
-											icon="pi pi-plus"
-											severity="secondary"
-											class="dark:!bg-dark-800"
-											outlined
-											@click="addTag"
-										/>
-										<Button
-											label="Save"
-											icon="pi pi-check"
-											severity="secondary"
-											outlined
-											class="ml-auto bg-surface-200"
-											@click="saveTags(slotProps.data.id)"
-										/>
-										<Button label="Cancel" severity="secondary" outlined class="ml-1 dark:!bg-dark-800" @click="cancelTags"/>
-									</div>
-									<p class="mt-3 text-2xs text-bluegray-400">
-										Public tags of the probe. They can be used as location filters for a measurement. Format is <code class="font-bold">u-${prefix}-${value}</code> where prefix is user/organization github login, and value is your custom string.
-
-										E.g. for user with github username <code class="font-bold">"jimaek"</code>
-										and tag <code class="font-bold">"home1"</code> location filter is<br>
-										<code class="font-bold">{ "tags": ["u-jimaek-home1"] }</code>.
-									</p>
-								</div>
-								<div v-else>
-									<div>
-										<Tag v-for="tag in slotProps.data.tags" :key="tag" class="my-0.5 mr-1 flex bg-surface-0 py-0.5 font-normal dark:bg-dark-800" severity="secondary" :value="`${tag.prefix}-${tag.value}`"/>
-									</div>
-									<Button
-										class="mt-3"
-										label="Edit tags"
-										icon="pi pi-pencil"
-										severity="secondary"
-										outlined
-										@click="editTags(slotProps.data.tags)"
-									/>
-									<p class="mt-3 text-2xs text-bluegray-400">Public tags of the probe. They can be used as location filters for a measurement.</p>
-								</div>
-							</div>
-							<div class="mt-6 w-[145%] px-2">
-								<p class="border-b pb-2 font-bold">Tests (last 24h)</p>
-								<div class="pb-4 pt-8 text-center">
-									<i class="pi pi-chart-bar py-4 text-lg text-primary"/>
-									<p class="font-bold">Tests data coming soon.</p>
-									<p>Here you will see the probe tests statistics.</p>
-								</div>
-							</div>
-						</div>
-						<div v-else class="px-2 py-4">
+						<div class="px-2 py-4">
 							<Tag v-if="slotProps.data.tags[0]" :key="slotProps.data.tags[0]" class="my-0.5 mr-1 flex bg-surface-0 py-0.5 font-normal dark:bg-dark-800" severity="secondary" :value="`${slotProps.data.tags[0].prefix}-${slotProps.data.tags[0].value}`"/>
 							<Tag v-if="slotProps.data.tags[1]" :key="slotProps.data.tags[1]" class="my-0.5 mr-1 flex bg-surface-0 py-0.5 font-normal dark:bg-dark-800" severity="secondary" :value="`${slotProps.data.tags[1].prefix}-${slotProps.data.tags[1].value}`"/>
 							<Tag v-if="slotProps.data.tags.length > 2" key="other" class="my-0.5 mr-1 flex bg-surface-0 py-0.5 font-normal dark:bg-dark-800" severity="secondary" :value="`+${slotProps.data.tags.length - 2}`"/>
@@ -226,17 +66,8 @@
 					</template>
 				</Column>
 				<Column expander class="w-[3%]" body-class="!p-0">
-					<template #body="slotProps">
-						<div
-							v-if="expandedRow === slotProps.data.id"
-							class="cursor-pointer px-2 py-5"
-							tabindex="0"
-							@click.stop="expandedRow = 0"
-							@keydown.stop="onKeyDown"
-						>
-							<i class="pi pi-chevron-down"/>
-						</div>
-						<div v-else class="px-2 py-4">
+					<template #body="">
+						<div class="px-2 py-4">
 							<i class="pi pi-chevron-right"/>
 						</div>
 					</template>
@@ -344,8 +175,7 @@
 	const credits = ref<Record<string, number>>({});
 	const first = ref(0);
 	const lazyParams = ref<Partial<DataTablePageEvent>>({});
-	const expandedRow = ref<number>(0);
-	const expandedRows = computed(() => ({ [expandedRow.value]: true }));
+	const openedRow = ref<number>(0);
 	const totalCredits = ref(0);
 
 	const loadLazyData = async (event?: PageState) => {
@@ -413,13 +243,13 @@
 
 	// PROBE DETAILS
 
-	const toggleRow = (event: DataTableRowClickEvent) => {
-		if (event.data.id !== expandedRow.value) {
+	const openRow = (event: DataTableRowClickEvent) => {
+		if (event.data.id !== openedRow.value) {
 			isEditingName.value = false;
 			isEditingCity.value = false;
 			isEditingTags.value = false;
-			expandedRow.value = event.data.id;
-			const probe = probes.value.find(probe => probe.id === expandedRow.value);
+			openedRow.value = event.data.id;
+			const probe = probes.value.find(probe => probe.id === openedRow.value);
 			probe && initGoogleMap(probe);
 		}
 	};
@@ -515,7 +345,7 @@
 
 	const onKeyDown = async (event: KeyboardEvent) => {
 		if (event.key === 'Enter' || event.key === ' ') {
-			expandedRow.value = 0;
+			openedRow.value = 0;
 		}
 	};
 

8e7acf88e67af2bf600570aa3048188f58a538cd
Martin Kolárik <martin@kolarik.sk>
2024-10-02 15:09:11 +0200

    misc: reorganize the settings page

--- pages/settings.vue
+++ pages/settings.vue
@@ -10,8 +10,13 @@
 			<div class="grow">
 				<label for="firstName" class="block font-bold">First Name</label>
 				<InputText id="firstName" v-model="firstName" class="mt-2 w-full"/>
+
 				<label for="lastName" class="mt-6 block font-bold">Last Name</label>
 				<InputText id="lastName" v-model="lastName" class="mt-2 w-full"/>
+
+				<label for="email" class="mt-6 block font-bold">Email</label>
+				<InputText id="email" v-model="email" class="mt-2 w-full"/>
+
 				<label for="username" class="mt-6 block font-bold">Username</label>
 				<div class="relative mt-2">
 					<Button
@@ -25,8 +30,38 @@
 					<i class="pi pi-lock absolute right-3 top-2.5 text-bluegray-500"/>
 					<InputText id="username" v-model="user.github_username" disabled class="pointer-events-auto w-full cursor-text select-auto bg-transparent pr-44 dark:bg-transparent"/>
 				</div>
+
+				<label for="organizations" class="mt-6 block font-bold">Organizations</label>
+				<div class="relative mt-2">
+					<ReadOnlyAutoComplete
+						id="tags"
+						v-model="user.github_organizations"
+						:pt="{
+							inputMultiple: { class: 'pb-1 pr-48 min-h-10' },
+							inputChip: 'hidden',
+							chipItem: 'mt-1'
+						}"
+					/>
+					<Button
+						severity="contrast"
+						size="small"
+						text
+						label="Sync from GitHub"
+						:icon="loadingIconId === 2 ? 'pi pi-sync pi-spin' : 'pi pi-sync'"
+						class="!absolute right-8 top-2 h-6 bg-transparent !px-1 hover:bg-transparent"
+						@click="syncFromGithub(2)"
+					/>
+					<i class="pi pi-lock absolute right-3 top-3 text-bluegray-500"/>
+				</div>
+
+				<label for="userType" class="mt-6 block font-bold">User type</label>
+				<div class="relative mt-2">
+					<i class="pi pi-lock absolute right-3 top-2.5 text-bluegray-500"/>
+					<InputText id="userType" v-model="user.user_type" disabled class="pointer-events-auto w-full cursor-text select-auto bg-transparent pr-44 dark:bg-transparent"/>
+				</div>
 			</div>
 		</div>
+
 		<div class="mt-6 flex rounded-xl border bg-surface-0 p-6 max-sm:flex-col dark:bg-dark-800">
 			<div class="max-sm:mb-4 sm:w-2/5">
 				<h5 class="text-lg font-bold">Interface</h5>
@@ -50,42 +85,7 @@
 				</SelectButton>
 			</div>
 		</div>
-		<div class="mt-6 flex rounded-xl border bg-surface-0 p-6 max-sm:flex-col dark:bg-dark-800">
-			<div class="max-sm:mb-4 sm:w-2/5">
-				<h5 class="text-lg font-bold">Other</h5>
-			</div>
-			<div class="grow">
-				<label for="email" class="block font-bold">Email</label>
-				<InputText id="email" v-model="email" class="mt-2 w-full"/>
-				<label for="organizations" class="mt-6 block font-bold">Organizations</label>
-				<div class="relative mt-2">
-					<ReadOnlyAutoComplete
-						id="tags"
-						v-model="user.github_organizations"
-						:pt="{
-							inputMultiple: { class: 'pb-1 pr-48 min-h-10' },
-							inputChip: 'hidden',
-							chipItem: 'mt-1'
-						}"
-					/>
-					<Button
-						severity="contrast"
-						size="small"
-						text
-						label="Sync from GitHub"
-						:icon="loadingIconId === 2 ? 'pi pi-sync pi-spin' : 'pi pi-sync'"
-						class="!absolute right-8 top-2 h-6 bg-transparent !px-1 hover:bg-transparent"
-						@click="syncFromGithub(2)"
-					/>
-					<i class="pi pi-lock absolute right-3 top-3 text-bluegray-500"/>
-				</div>
-				<label for="userType" class="mt-6 block font-bold">User type</label>
-				<div class="relative mt-2">
-					<i class="pi pi-lock absolute right-3 top-2.5 text-bluegray-500"/>
-					<InputText id="userType" v-model="user.user_type" disabled class="pointer-events-auto w-full cursor-text select-auto bg-transparent pr-44 dark:bg-transparent"/>
-				</div>
-			</div>
-		</div>
+
 		<div class="mt-6 flex rounded-xl border bg-surface-0 p-6 max-sm:flex-col dark:bg-dark-800">
 			<div class="max-sm:mb-4 sm:w-2/5">
 				<h5 class="text-lg font-bold">Data removal</h5>

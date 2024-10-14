9ae485b4ebc1d2d42e2139da1fdb4c9e23f2d602
Alexey Yarmosh <yarmoshalexey@gmail.com>
2024-10-14 18:09:07 +0200

    fix: adopted probe flag position

--- components/AdoptProbe.vue
+++ components/AdoptProbe.vue
@@ -126,11 +126,8 @@
 						</p>
 						<p class="mt-4 text-center">You are now the owner of the following probe:</p>
 						<div v-if="probe" class="mt-4 rounded-xl border bg-surface-0 p-3 text-center dark:border-dark-400 dark:bg-dark-800">
-							<p class="font-bold">{{ probe.city }}</p>
-							<p class="flex items-center justify-center">
-								<CountryFlag :country="probe.country" size="small"/>
-								<span class="ml-2">{{ probe.network }}</span>
-							</p>
+							<p class="flex items-center justify-center font-bold"><CountryFlag :country="probe.country" size="small"/><span class="ml-2">{{ probe.city }}</span></p>
+							<p>{{ probe.network }}</p>
 						</div>
 					</div>
 					<p class="mt-4">The probe will generate credits that you can use to run more tests. We also recommend you verify and correct the probe's location.</p>

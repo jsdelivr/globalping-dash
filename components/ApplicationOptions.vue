8055affb1bba0e81ae5471da8cd9450bd11b2aa7
Alexey Yarmosh <yarmoshalexey@gmail.com>
2024-12-27 16:42:39 +0100

    feat: add apps table

---
+++ components/ApplicationOptions.vue
@@ -0,0 +1,33 @@
+<template>
+	<div>
+		<Button
+			icon="pi pi-ellipsis-h"
+			text
+			severity="secondary"
+			aria-label="Options"
+			aria-haspopup="true"
+			aria-controls="overlay_tmenu"
+			@click="toggle"
+		/>
+		<TieredMenu id="overlay_tmenu" ref="menu" :model="items" popup/>
+	</div>
+</template>
+
+<script setup lang="ts">
+	const emit = defineEmits([ 'revoke' ]);
+
+	const menu = ref();
+	const items = ref([
+		{
+			label: 'Revoke access',
+			icon: 'pi pi-undo',
+			command: () => emit('revoke'),
+			class: '!text-red-500',
+			style: 'color: red;',
+		},
+	]);
+
+	const toggle = (event: Event) => {
+		menu.value.toggle(event);
+	};
+</script>
--- types.ts
+++ types.ts
@@ -13,6 +13,7 @@ declare global {
     gp_credits_additions: CreditsAddition[];
     gp_credits_deductions: CreditsDeduction[];
     gp_tokens: Token[];
+    gp_apps: Application[];
 
     directus_users: User;
   };
@@ -98,6 +99,27 @@ declare global {
     value: string;
     user_created: string;
     user_updated: string | null;
+    app_id: string | null;
+    scopes: string[];
+    type: string;
+    parent: number | null;
+  };
+
+  type Application = {
+    id: string;
+    user_created: string | null;
+    date_created: string | null;
+    user_updated: string | null;
+    date_updated: string | null;
+    name: string;
+    redirect_urls: string[];
+    urls: string[];
+    grants: string[];
+    access_token_lifetime: number | null;
+    refresh_token_lifetime: number | null;
+    owner_name: string | null;
+    owner_url: string | null;
+    secrets: string[];
   };
 
   type DashboardError = {

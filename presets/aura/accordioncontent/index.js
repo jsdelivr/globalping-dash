023f5b8a664b11992eb0d006b0a6611ceb7514b9
xbpcb <ch.kyrill@gmail.com>
2025-01-23 19:10:40 +0200

    Accordion: mod content classes

--- presets/aura/accordioncontent/index.js
+++ presets/aura/accordioncontent/index.js
@@ -1,3 +1,5 @@
 export default {
-	content: 'p-6 pb-5 pt-0 text-bluegray-600 dark:text-surface-0',
+	// TODO: 43, it was set for notification dropdown, but it messes with other places now
+	// content: 'p-6 pb-5 pt-0 text-bluegray-600 dark:text-surface-0',
+	content: '',
 };

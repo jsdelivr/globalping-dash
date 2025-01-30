dd6e7363637fecf15964de944f3ebe3712a271b1
xbpcb <ch.kyrill@gmail.com>
2025-01-30 21:37:09 +0200

    Notif: hide markAllAsRead btn when no notifications

--- pages/notifications.vue
+++ pages/notifications.vue
@@ -3,6 +3,7 @@
 		<div class="flex h-10 items-center justify-between">
 			<h1 class="text-2xl font-bold leading-8">Your notifications</h1>
 			<Button
+				v-if="displayedNotifications.length"
 				class="flex h-10 items-center gap-x-2 border border-solid !border-[var(--p-surface-300)] bg-white !text-bluegray-900 hover:!border-[var(--p-primary-500)] hover:!bg-[var(--p-primary-500)] hover:!text-white"
 				@click="markAllNotificationsAsRead()"
 			>

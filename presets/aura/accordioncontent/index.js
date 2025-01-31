bbf89b7ef625484e26dead1e2d1f143fef9a4694
xbpcb <ch.kyrill@gmail.com>
2025-01-31 17:05:11 +0200

    Linter fix

--- presets/aura/accordioncontent/index.js
+++ presets/aura/accordioncontent/index.js
@@ -6,11 +6,11 @@ export default {
 		],
 	},
 	transition: {
-    enterFromClass: 'max-h-0',
-    enterActiveClass: 'overflow-hidden transition-[max-height] duration-1000 ease-[cubic-bezier(0.42,0,0.58,1)]',
-    enterToClass: 'max-h-[1000px]',
-    leaveFromClass: 'max-h-[1000px]',
-    leaveActiveClass: 'overflow-hidden transition-[max-height] duration-[450ms] ease-[cubic-bezier(0,1,0,1)]',
-    leaveToClass: 'max-h-0',
-  },
+		enterFromClass: 'max-h-0',
+		enterActiveClass: 'overflow-hidden transition-[max-height] duration-1000 ease-[cubic-bezier(0.42,0,0.58,1)]',
+		enterToClass: 'max-h-[1000px]',
+		leaveFromClass: 'max-h-[1000px]',
+		leaveActiveClass: 'overflow-hidden transition-[max-height] duration-[450ms] ease-[cubic-bezier(0,1,0,1)]',
+		leaveToClass: 'max-h-0',
+	},
 };

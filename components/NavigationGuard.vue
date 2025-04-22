0b777a438ae05265d63f92a6858090cc018598b5
Martin Kolárik <martin@kolarik.sk>
2025-04-22 16:57:31 +0200

    fix: imports

--- components/NavigationGuard.vue
+++ components/NavigationGuard.vue
@@ -19,8 +19,7 @@
 </template>
 
 <script setup lang="ts">
-	import { ref, inject, onMounted, onUnmounted, type Ref } from 'vue';
-	import { useRouter, type RouteLocationNormalized, type NavigationGuardNext } from 'vue-router';
+	import { type RouteLocationNormalized, type NavigationGuardNext } from 'vue-router';
 
 	const router = useRouter();
 	const showNavigationConfirm = ref(false);

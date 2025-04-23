b7dd12f744312694cba2b9607ccce48fce338e0e
xbpcb <ch.kyrill@gmail.com>
2025-04-24 00:24:34 +0300

    TrackFrom: add middleware descr

--- middleware/track-from.ts
+++ middleware/track-from.ts
@@ -1,3 +1,7 @@
+/**
+	Middleware to track the previous route's full path.
+	Used to support back navigation.
+*/
 export default defineNuxtRouteMiddleware((to, from) => {
 	const fromPath = useState<string | null>('fromPath', () => null);
 

40d0fdd706e036935e929080228eb647c0caa9cc
xbpcb <ch.kyrill@gmail.com>
2025-04-23 22:54:21 +0300

    ProbeDetails: make tabs focusable

--- pages/probes/[id].vue
+++ pages/probes/[id].vue
@@ -153,8 +153,8 @@
 
 			<Tabs value="0">
 				<TabList class="!border-b !border-surface-300 dark:!border-dark-600">
-					<Tab value="0" class="!w-1/2 border-none !px-6 !py-2 !text-[14px] !font-bold sm:!w-auto">Details</Tab>
-					<Tab value="1" class="!w-1/2 border-none !px-6 !py-2 !text-[14px] !font-bold sm:!w-auto">Logs</Tab>
+					<Tab value="0" tabindex="0" class="!w-1/2 border-none !px-6 !py-2 !text-[14px] !font-bold sm:!w-auto">Details</Tab>
+					<Tab value="1" tabindex="0" class="!w-1/2 border-none !px-6 !py-2 !text-[14px] !font-bold sm:!w-auto">Logs</Tab>
 				</TabList>
 
 				<TabPanels class="mt-6 !bg-transparent !p-0">

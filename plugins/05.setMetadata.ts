aeb73f8e754672be3d03d9be545a800cd1709f25
Martin Kolárik <martin@kolarik.sk>
2025-05-18 01:12:31 +0200

    fix: fetch metadata only when logged in

--- plugins/05.setMetadata.ts
+++ plugins/05.setMetadata.ts
@@ -1,9 +1,15 @@
 import { customEndpoint } from '@directus/sdk';
+import { useAuth } from '~/store/auth.js';
 import { useMetadata } from '~/store/metadata.js';
 
 export default defineNuxtPlugin(async () => {
+	const auth = useAuth();
 	const metadata = useMetadata();
 	const { $directus } = useNuxtApp();
+
+	if (!auth.isLoggedIn) {
+		return;
+	}
 
 	const response = await $directus.request<Metadata>(customEndpoint({ method: 'GET', path: '/metadata' }));
 	metadata.setMetadata(response);

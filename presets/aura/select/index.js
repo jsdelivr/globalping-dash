33765a4902b2f942d12bbb84d30fc8176371e08e
Martin Kolárik <martin@kolarik.sk>
2025-06-15 15:18:10 +0200

    style: update comment indentation in aura select presets

--- presets/aura/select/index.js
+++ presets/aura/select/index.js
@@ -137,9 +137,9 @@ export default {
 			// Colors
 			{
 			// 	'text-surface-700 dark:text-white/80': (!context.focused && !context.selected) || (context.focused && !context.selected),
-			// 'bg-surface-200 dark:bg-surface-600/60': context.focused && !context.selected,
+			//  'bg-surface-200 dark:bg-surface-600/60': context.focused && !context.selected,
 				'bg-surface-100 dark:text-surface-0 dark:bg-dark-600': context.focused && !context.selected,
-			// 	// 'text-surface-700 dark:text-white/80': context.focused && !context.selected,
+			// 	'text-surface-700 dark:text-white/80': context.focused && !context.selected,
 			// 	'bg-highlight': context.selected,
 			},
 

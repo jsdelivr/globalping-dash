268b442eeba4ecec7a07314f2b5c6e918d3f8e1e
Pavel Kopecký <pavel.kopecky@icloud.com>
2025-09-16 13:05:49 +0200

    fix: preserve computedDebounced source type

--- composables/computedDebounced.ts
+++ composables/computedDebounced.ts
@@ -1,4 +1,4 @@
-export const computedDebounced = (source: MaybeRefOrGetter<unknown>, ms: number = 1, maxWait: number = 1000) => {
+export const computedDebounced = <T>(source: MaybeRefOrGetter<T>, ms: number = 1, maxWait: number = 1000) => {
 	const dependency = computed(() => toValue(source));
 
 	return refDebounced(dependency, ms, {

0db8f186fc334f72abdd5fd93ceae1a339b557f8
Pavel Kopecký <pavel.kopecky@icloud.com>
2025-09-28 13:21:47 +0200

    fix: 500 when directus is down

--- app/plugins/05.setMetadata.ts
+++ app/plugins/05.setMetadata.ts
@@ -5,6 +5,10 @@ export default defineNuxtPlugin(async () => {
 	const metadata = useMetadata();
 	const { $directus } = useNuxtApp();
 
-	const response = await $directus.request<Metadata>(customEndpoint({ method: 'GET', path: '/metadata' }));
-	metadata.setMetadata(response);
+	try {
+		const response = await $directus.request<Metadata>(customEndpoint({ method: 'GET', path: '/metadata' }));
+		metadata.setMetadata(response);
+	} catch (e) {
+		console.error(e);
+	}
 });

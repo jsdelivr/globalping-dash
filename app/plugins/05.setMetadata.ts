dab88febe9706344c99aa6aca216d5be2d029f11
Pavel Kopecký <pavel.kopecky@icloud.com>
2025-09-30 11:51:07 +0200

    Revert "fix: 500 when directus is down"
    This reverts commit 0db8f186fc334f72abdd5fd93ceae1a339b557f8.

--- app/plugins/05.setMetadata.ts
+++ app/plugins/05.setMetadata.ts
@@ -5,10 +5,6 @@ export default defineNuxtPlugin(async () => {
 	const metadata = useMetadata();
 	const { $directus } = useNuxtApp();
 
-	try {
-		const response = await $directus.request<Metadata>(customEndpoint({ method: 'GET', path: '/metadata' }));
-		metadata.setMetadata(response);
-	} catch (e) {
-		console.error(e);
-	}
+	const response = await $directus.request<Metadata>(customEndpoint({ method: 'GET', path: '/metadata' }));
+	metadata.setMetadata(response);
 });

6a7cf0cde745a29e143c1387aa51d3e8e798bad7
Pavel Kopecký <pavel.kopecky@icloud.com>
2025-10-06 19:28:27 +0200

    feat: improve credits table on mobile devices (#146)

---
+++ app/components/list-items/CreditItem.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="flex min-h-28 w-full flex-col justify-between gap-2 rounded-lg border px-4 py-3">
+		<div class="flex justify-between gap-6">
+			<b>Date</b>
+			<span>{{ creditChange.date_created }}</span>
+		</div>
+
+		<div class="flex justify-between gap-6">
+			<b>Comment</b>
+			<span class="truncate">{{ formatCreditComment(creditChange) }}</span>
+		</div>
+
+		<div class="flex justify-between gap-6">
+			<b>Amount</b>
+			<Tag v-if="creditChange.type === 'addition'" class="flex items-center !text-sm" severity="success">
+				<NuxtIcon class="mr-2" name="coin" aria-hidden="true"/>+{{ (creditChange.amount || 0).toLocaleString('en-US') }}
+			</Tag>
+			<Tag v-else class="flex items-center !text-sm" severity="danger">
+				<NuxtIcon class="mr-2" name="coin" aria-hidden="true"/>-{{ (creditChange.amount || 0).toLocaleString('en-US') }}
+			</Tag>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+	import { formatCreditComment } from '~/utils/format-credit-comment';
+
+	const { creditChange } = defineProps({
+		creditChange: {
+			type: Object as PropType<CreditsChange>,
+			required: true,
+		},
+	});
+</script>
---
+++ app/utils/format-credit-comment.ts
@@ -0,0 +1,18 @@
+export const formatCreditComment = (change: CreditsChange) => {
+	if (change.type === 'deduction') {
+		return 'Measurements ran on this day.';
+	}
+
+	switch (change.reason) {
+		case 'one_time_sponsorship':
+			return `One-time $${change.meta.amountInDollars} sponsorship.`;
+		case 'recurring_sponsorship':
+			return `Recurring $${change.meta.amountInDollars} sponsorship.`;
+		case 'tier_changed':
+			return `Sponsorship tier changed. Adding a diff of $${change.meta.amountInDollars}.`;
+		case 'adopted_probe':
+			return `Adopted probes.`;
+		default:
+			return change.meta?.comment || 'Other';
+	}
+};

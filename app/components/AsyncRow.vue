84c7fef553b3a0696be20a7698fa9162a1bedbde
Pavel Kopecký <pavel.kopecky@icloud.com>
2025-10-07 17:46:15 +0200

    feat: improve mobile version of the tokens page (#147)

---
+++ app/components/AsyncRow.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="relative">
+		<div :class="{ 'opacity-0 transition-none': loading }">
+			<slot/>
+		</div>
+		<Skeleton v-show="loading" width="100%" height="100%" class="!absolute inset-0 dark:opacity-20"/>
+	</div>
+</template>
+
+<script setup lang="ts">
+	defineProps({
+		loading: {
+			type: Boolean,
+			required: true,
+		},
+	});
+</script>

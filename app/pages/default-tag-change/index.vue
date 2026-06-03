342052371613a37560a315381a28b30b202ec3de
Alexey Yarmosh <yarmoshalexey@gmail.com>
2026-06-03 13:31:28 +0200

    misc: move page

---
+++ app/pages/default-tag-change/index.vue
@@ -0,0 +1,70 @@
+<template>
+	<div>
+		<div v-if="isConfirmed">
+			<Message severity="success" class="font-normal">
+				<p>Your default probe tag change has been confirmed.</p>
+			</Message>
+			<p class="mt-4">You can change your tag prefix any time on the <NuxtLink to="/settings" class="underline">settings page</NuxtLink>.</p>
+		</div>
+		<div v-else>
+			<h3>Confirm your default probe tag change?</h3>
+			<p class="mt-4">Confirming stops your previous default tag from being used to target your probes. You can change your tag prefix any time on the <NuxtLink to="/settings" class="underline">settings page</NuxtLink>.</p>
+			<Divider class="h-2 before:border-t-2"/>
+
+			<div class="flex gap-2">
+				<NuxtLink to="/settings" class="w-1/2">
+					<Button
+						class="h-12 w-full bg-black"
+						severity="secondary"
+						label="Configure"
+					/>
+				</NuxtLink>
+
+				<Button
+					class="h-12 w-1/2 bg-black"
+					severity="contrast"
+					label="Confirm"
+					:loading="isSubmitting"
+					:disabled="!confirmData"
+					@click="confirm"
+				/>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+	import { customEndpoint } from '@directus/sdk';
+	import { sendErrorToast } from '~/utils/send-toast';
+
+	definePageMeta({
+		layout: 'info',
+	});
+
+	useHead({
+		title: 'Confirm prefix change -',
+	});
+
+	const { $directus } = useNuxtApp();
+	const route = useRoute();
+	const isSubmitting = ref(false);
+	const isConfirmed = ref(false);
+	const confirmData = computed(() => typeof route.query.data === 'string' ? route.query.data.trim() : '');
+
+	async function confirm () {
+		if (!confirmData.value || isSubmitting.value || isConfirmed.value) {
+			return;
+		}
+
+		isSubmitting.value = true;
+
+		try {
+			await $directus.request(customEndpoint({ method: 'POST', path: '/sync-github-data/default-tag-change', params: { data: confirmData.value } }));
+			isConfirmed.value = true;
+		} catch (err) {
+			sendErrorToast(err);
+		} finally {
+			isSubmitting.value = false;
+		}
+	}
+</script>

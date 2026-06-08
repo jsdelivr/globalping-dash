aedd3826e398c34cf48057e6e7c143acbe1a2755
Pavel Kopecký <pavel.kopecky@icloud.com>
2026-06-08 12:42:36 +0200

    fix: catch setMetadata error on the login page

--- app/plugins/05.setMetadata.ts
+++ app/plugins/05.setMetadata.ts
@@ -1,10 +1,23 @@
 import { customEndpoint } from '@directus/sdk';
+import { useAuth } from '~/store/auth';
 import { useMetadata } from '~/store/metadata';
 
+const INFO_PAGES = [ 'emails', 'authorize' ];
+
 export default defineNuxtPlugin(async () => {
+	const auth = useAuth();
+	const route = useRoute();
 	const metadata = useMetadata();
 	const { $directus } = useNuxtApp();
 
-	const response = await $directus.request<Metadata>(customEndpoint({ method: 'GET', path: '/metadata' }));
-	metadata.setMetadata(response);
+	try {
+		const response = await $directus.request<Metadata>(customEndpoint({ method: 'GET', path: '/metadata' }));
+		metadata.setMetadata(response);
+	} catch (error) {
+		if (!auth.isLoggedIn && !INFO_PAGES.some(page => (route.name as string).startsWith(page))) {
+			return;
+		}
+
+		throw error;
+	}
 });

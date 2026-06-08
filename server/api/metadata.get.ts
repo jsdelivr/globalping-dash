f14bf7a377e1ba0d4e079e4821c7604b62626364
Martin Kolárik <martin@kolarik.sk>
2026-06-08 22:35:44 +0200

    fix: add retry and timeout options to metadata API request

--- server/api/metadata.get.ts
+++ server/api/metadata.get.ts
@@ -1,7 +1,10 @@
 export default defineCachedEventHandler(async (event) => {
 	const config = useRuntimeConfig(event);
 
-	return $fetch<Metadata>(`${config.public.directusUrl}/metadata`);
+	return $fetch<Metadata>(`${config.public.directusUrl}/metadata`, {
+		retry: 1,
+		timeout: 5000,
+	});
 }, {
 	maxAge: 60,
 	name: 'directus-metadata',

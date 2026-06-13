8c96dbcb824550fe3d02803bccfc7dfa945376a4
Martin Kolárik <martin@kolarik.sk>
2026-06-14 00:51:37 +0200

    fix: disable cache control for previews

--- server/plugins/cache-control.ts
+++ server/plugins/cache-control.ts
@@ -1,10 +1,15 @@
 export default defineNitroPlugin((nitroApp) => {
+	const config = useRuntimeConfig();
+
 	nitroApp.hooks.hook('render:response', (response) => {
 		const contentType = response.headers?.['content-type'];
 
 		if (typeof contentType === 'string' && contentType.startsWith('text/html')) {
 			response.headers ||= {};
-			response.headers['cache-control'] = 'public, max-age=300';
+
+			response.headers['cache-control'] = config.isCoolifyPreview
+				? 'no-cache, no-store'
+				: 'public, max-age=300';
 		}
 	});
 });
